23a045023d9cc132961cd539926b8e667ad03a0c
Anurag <anurag.sritharan@desy.de>
2024-11-05 21:53:29 +0100

    one last cleanup to go

--- minimization.py
+++ minimization.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class NegativeLogLikelihood(object):
     """
     Class containing minimisation statistic to be for pdf fitting. The class takes in data as the input.
@@ -10,19 +11,18 @@ class NegativeLogLikelihood(object):
         self.pdf = pdf
         self.data = data
 
-
     def evaluate_gaussian(self, mean, sigma):
         """
         Evaluate negative log likelihood for gaussian signal and linear background
         """
 
-        #This is where the params def takes effect as we can now set different parameters as we want for the minimisation
+        # This is where the params def takes effect as we can now set different parameters as we want for the minimisation
         self.pdf.params(mean=mean, sigma=sigma)
         norm = self.pdf.integrate((self.pdf.XMIN, self.pdf.XMAX))
-        evaluate_likelihood = self.pdf.evaluate(self.data,)/norm 
+        evaluate_likelihood = self.pdf.evaluate(self.data,) / norm
         # fudge factor for negative or 0 values. These are set very close to 0
-        if (evaluate_likelihood <= 0).any():
-            evaluate_likelihood[evaluate_likelihood<=0 ] = 1e-5
+        if np.any(evaluate_likelihood <= 0):
+            evaluate_likelihood[evaluate_likelihood <= 0] = 1e-5
         NLL = np.log(evaluate_likelihood)
         return -NLL.sum()
 
@@ -31,13 +31,13 @@ class NegativeLogLikelihood(object):
         Evaluate negative log likelihood for gaussian signal and linear background
         """
 
-        #This is where the params def takes effect as we can now set different parameters as we want for the minimisation
-        self.pdf.params(decay_constant = decay_constant)
+        # This is where the params def takes effect as we can now set different parameters as we want for the minimisation
+        self.pdf.params(decay_constant=decay_constant)
         norm = self.pdf.integrate((self.pdf.XMIN, self.pdf.XMAX))
-        evaluate_likelihood = self.pdf.evaluate(self.data,)/norm 
+        evaluate_likelihood = self.pdf.evaluate(self.data,) / norm
         # fudge factor for negative or 0 values. These are set very close to 0
-        if (evaluate_likelihood <= 0).any():
-            evaluate_likelihood[evaluate_likelihood<=0 ] = 1e-5
+        if np.any(evaluate_likelihood <= 0):
+            evaluate_likelihood[evaluate_likelihood <= 0] = 1e-5
         NLL = np.log(evaluate_likelihood)
         return -NLL.sum()
 
@@ -46,13 +46,13 @@ class NegativeLogLikelihood(object):
         Evaluate negative log likelihood for gaussian signal and linear background
         """
 
-        #This is where the params def takes effect as we can now set different parameters as we want for the minimisation
-        self.pdf.params(slope = slope)
+        # This is where the params def takes effect as we can now set different parameters as we want for the minimisation
+        self.pdf.params(slope=slope)
         norm = self.pdf.integrate((self.pdf.XMIN, self.pdf.XMAX))
-        evaluate_likelihood = self.pdf.evaluate(self.data,)/norm 
+        evaluate_likelihood = self.pdf.evaluate(self.data,) / norm
         # fudge factor for negative or 0 values. These are set very close to 0
-        if (evaluate_likelihood <= 0).any():
-            evaluate_likelihood[evaluate_likelihood<=0 ] = 1e-5
+        if np.any(evaluate_likelihood <= 0):
+            evaluate_likelihood[evaluate_likelihood <= 0] = 1e-5
         NLL = np.log(evaluate_likelihood)
         return -NLL.sum()
 
@@ -61,28 +61,28 @@ class NegativeLogLikelihood(object):
         Evaluate negative log likelihood for gaussian signal and linear background
         """
 
-        #This is where the params def takes effect as we can now set different parameters as we want for the minimisation
-        self.pdf.params(y_int = y_int)
+        # This is where the params def takes effect as we can now set different parameters as we want for the minimisation
+        self.pdf.params(y_int=y_int)
         norm = self.pdf.integrate((self.pdf.XMIN, self.pdf.XMAX))
-        evaluate_likelihood = self.pdf.evaluate(self.data,)/norm 
+        evaluate_likelihood = self.pdf.evaluate(self.data,) / norm
         # fudge factor for negative or 0 values. These are set very close to 0
-        if (evaluate_likelihood <= 0).any():
-            evaluate_likelihood[evaluate_likelihood<=0 ] = 1e-5
+        if np.any(evaluate_likelihood <= 0):
+            evaluate_likelihood[evaluate_likelihood <= 0] = 1e-5
         NLL = np.log(evaluate_likelihood)
         return -NLL.sum()
 
-    def evaluate_polynomial(self,  a, b, c):
+    def evaluate_polynomial(self, a, b, c):
         """
         Evaluate negative log likelihood for gaussian signal and linear background
         """
 
-        #This is where the params def takes effect as we can now set different parameters as we want for the minimisation
-        self.pdf.params( a = a, b = b, c = c)
+        # This is where the params def takes effect as we can now set different parameters as we want for the minimisation
+        self.pdf.params(a=a, b=b, c=c)
         norm = self.pdf.integrate((self.pdf.XMIN, self.pdf.XMAX))
-        evaluate_likelihood = self.pdf.evaluate(self.data,)/norm 
+        evaluate_likelihood = self.pdf.evaluate(self.data,) / norm
         # fudge factor for negative or 0 values. These are set very close to 0
-        if (evaluate_likelihood <= 0).any():
-            evaluate_likelihood[evaluate_likelihood<=0 ] = 1e-5
+        if np.any(evaluate_likelihood <= 0):
+            evaluate_likelihood[evaluate_likelihood <= 0] = 1e-5
         NLL = np.log(evaluate_likelihood)
         return -NLL.sum()
 
@@ -96,11 +96,11 @@ class NegativeLogLikelihood(object):
 
         #  compute the normalisation factor and the likelihood
         normalisation = self.pdf.integrate((self.pdf.XMIN, self.pdf.XMAX))
-        evaluate_likelihood= self.pdf.evaluate(self.data,) / normalisation
-        
+        evaluate_likelihood = self.pdf.evaluate(self.data,) / normalisation
+
         # the fudge factor for values <=0. set them to a very small value close to 0
-        if (evaluate_likelihood<= 0).any():
-            evaluate_likelihood[evaluate_likelihood<=0 ] = 1e-5
+        if np.any(evaluate_likelihood <= 0):
+            evaluate_likelihood[evaluate_likelihood <= 0] = 1e-5
 
         # compute log and return thr negative sum
         return -(np.log(evaluate_likelihood).sum())
